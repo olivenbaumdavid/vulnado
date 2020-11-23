b01eb97d842d1f0a1c86c32e758a87e7afb8accd
David Olivenbaum <74703559+olivenbaumdavid@users.noreply.github.com>
2020-11-23 22:36:00 +0200

    fdsgdsgdsf

--- src/main/java/com/scalesec/vulnado/LoginController.java
+++ src/main/java/com/scalesec/vulnado/LoginController.java
@@ -18,7 +18,7 @@ public class LoginController {
   @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
   LoginResponse login(@RequestBody LoginRequest input) {
     User user = User.fetch(input.username);
-    User user2 = User.fetch2(input.username);
+    User user2 = User.fetch3(input.username);
     if (Postgres.md5(input.password).equals(user.hashedPassword)) {
       return new LoginResponse(user.token(secret));
     } else {
